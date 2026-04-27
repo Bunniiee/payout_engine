dd7fbc05d564942d37c09de92064499072fd4873
Playto <dev@playto.pay>
2026-04-28 02:14:46 +0530

    "debug-gunicorn-health-endpoint"

--- playto/urls.py
+++ playto/urls.py
@@ -1,5 +1,12 @@
+from django.http import HttpResponse
 from django.urls import path, include
 
+
+def health(request):
+    return HttpResponse('ok', content_type='text/plain')
+
+
 urlpatterns = [
+    path('health/', health),
     path('api/v1/', include('payouts.urls')),
 ]
--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py seed_db
 celery -A playto worker --beat --loglevel=info --concurrency=2 &
-gunicorn playto.wsgi --bind 0.0.0.0:$PORT --workers 2
+gunicorn playto.wsgi --bind 0.0.0.0:$PORT --workers 1 --timeout 120 --access-logfile - --log-level debug
