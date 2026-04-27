f05d8bfbbe02e6f734dcd1c00bddced0bb39a28b
Playto <dev@playto.pay>
2026-04-28 00:56:14 +0530

    "chore-auto-seed-on-startup"

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ set -e
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py seed_db
 celery -A playto worker --beat --loglevel=info --concurrency=2 &
 gunicorn playto.wsgi --bind 0.0.0.0:$PORT --workers 2
